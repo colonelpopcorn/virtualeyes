0a5254a85ddd037306f7923fd7e8ecc69ca6b961
Jonathan Ling <jonathan@code-server.linghome.net>
2020-09-02 11:54:18 +0000

    Add knobs to storybook.

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ Stencil components are just Web Components, so they work in any major framework 
 To start building a new web component using Stencil, clone this repo to a new directory:
 
 ```bash
-git clone https://github.com/ionic-team/stencil-component-starter.git my-component
-cd my-component
+git clone https://github.com/ionic-team/stencil-component-starter.git power-indicator
+cd power-indicator
 git remote rm origin
 ```
 
@@ -64,15 +64,15 @@ The first step for all three of these strategies is to [publish to NPM](https://
 
 ### Script tag
 
-- Put a script tag similar to this `<script src='https://unpkg.com/my-component@0.0.1/dist/mycomponent.js'></script>` in the head of your index.html
+- Put a script tag similar to this `<script src='https://unpkg.com/power-indicator@0.0.1/dist/mycomponent.js'></script>` in the head of your index.html
 - Then you can use the element anywhere in your template, JSX, html etc
 
 ### Node Modules
-- Run `npm install my-component --save`
-- Put a script tag similar to this `<script src='node_modules/my-component/dist/mycomponent.js'></script>` in the head of your index.html
+- Run `npm install power-indicator --save`
+- Put a script tag similar to this `<script src='node_modules/power-indicator/dist/mycomponent.js'></script>` in the head of your index.html
 - Then you can use the element anywhere in your template, JSX, html etc
 
 ### In a stencil-starter app
-- Run `npm install my-component --save`
-- Add an import to the npm packages `import my-component;`
+- Run `npm install power-indicator --save`
+- Add an import to the npm packages `import power-indicator;`
 - Then you can use the element anywhere in your template, JSX, html etc
--- src/components.d.ts
+++ src/components.d.ts
@@ -6,56 +6,36 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface PowerIndicator {
+        "color": string;
+        "isLoading": boolean;
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLPowerIndicatorElement extends Components.PowerIndicator, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLPowerIndicatorElement: {
+        prototype: HTMLPowerIndicatorElement;
+        new (): HTMLPowerIndicatorElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "power-indicator": HTMLPowerIndicatorElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface PowerIndicator {
+        "color"?: string;
+        "isLoading"?: boolean;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "power-indicator": PowerIndicator;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "power-indicator": LocalJSX.PowerIndicator & JSXBase.HTMLAttributes<HTMLPowerIndicatorElement>;
         }
     }
 }
